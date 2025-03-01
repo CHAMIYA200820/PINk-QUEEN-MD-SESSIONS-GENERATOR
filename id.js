6c3b54fe502b58e542beb43c9a5d41bc6c29b273
CHAMIYA200820 <chamindumax33333@gmail.com>
2025-03-01 14:31:23 +0530

    Update id.js

--- id.js
+++ id.js
@@ -1,17 +1,10 @@
-function makeid(num = 4, prefix = "PINk_QUEEN_MD_") {
+function makeid(num = 4) {
   let result = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let charactersLength = characters.length;
-
-  for (let i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  var characters9 = characters.length;
+  for (var i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters9));
   }
-
-  return prefix + result;
+  return result;
 }
-
-module.exports = { makeid };
-
-// Example Usage
-console.log(makeid(8)); // PINk_QUEEN_MD_A1b2C3d4
-console.log(makeid(6, "CUSTOM_")); // CUSTOM_XyZ123
+module.exports = {makeid};
